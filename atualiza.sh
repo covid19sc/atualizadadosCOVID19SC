c115151475ce9284f0541ae0131e7f569ddc921a
Luiz-Rafael Santos <lrsantos11@gmail.com>
2020-06-04 15:24:52 -0300

    updateto use last day

--- atualiza.sh
+++ atualiza.sh
@@ -3,21 +3,21 @@
 echo "Atualiza ICL Rt para página"
 ICLdir=$HOME/dev/covid19model
 ICLfigures=$HOME/dev/covid19model/Brazil/figures
-COVID19SCdir=$HOME/dev/covid19sc.github.io
+COVID19SCfigdir=$HOME/dev/atualizadadosCOVID19SC/figures
 
 # Run ICL Rscript for all brazilian states
 cd $ICLdir
-Rscript base-Brazil.r --full
-git commit -a -m "run ICL Brazil model in $(date +%Y-%m-%d-%T)"
+#Rscript base-Brazil.r --full
+#git commit -a -m "run ICL Brazil model in $(date +%Y-%m-%d-%T)"
 
 #  Copy SC Figure to covid19sc
 cd $ICLfigures
 latest=(SC*.png(.om[1]))
-fignewname=fig/resultado_ICL_SC_$(date +%Y-%m-%d).png
-cp -f  $latest $COVID19SCdir/$fignewname
+fignewname=resultado_ICL_SC_$(date --date="1 day ago" +%Y-%m-%d).png
+cp -f  $latest $COVID19SCfigdir/$fignewname
 
 # Upload figure to github
-cd $COVID19SCdir
+cd $COVID19SCfigdir
 git fetch --all
 git pull origin
 git add $fignewname
