12f30a1c22007ae44cdb5d481dd2b5d1f3bd0f92
Luiz-Rafael Santos <lrsantos11@gmail.com>
2020-06-08 15:14:30 -0300

    updates atualiza.sh to generate log.

--- atualiza.sh
+++ atualiza.sh
@@ -1,14 +1,16 @@
 #!/bin/zsh
 
-echo "Atualiza ICL Rt para página"
+LOGFILE=$HOME/dev/atualizadadosCOVID19SC/log/cron"$(date +%Y-%m-%d-%T)".log
+echo "$(date +%Y-%m-$d-%T) : Starting work\n"  >> $LOGFILE 2>&1
+echo "Atualiza ICL Rt para página" >> $LOGFILE 2>&1
 ICLdir=$HOME/dev/covid19model
 ICLfigures=$HOME/dev/covid19model/Brazil/figures
 COVID19SCfigdir=$HOME/dev/atualizadadosCOVID19SC/figures
 
 # Run ICL Rscript for all brazilian states
 cd $ICLdir
-Rscript base-Brazil.r --full
-git commit -a -m "run ICL Brazil model in $(date +%Y-%m-%d-%T)"
+Rscript base-Brazil.r --full >> $LOGFILE 2>&1
+git commit -a -m "run ICL Brazil model in $(date +%Y-%m-%d-%T)" >> $LOGFILE 2>&1
 
 #  Copy SC Figure to covid19sc
 cd $ICLfigures
@@ -22,4 +24,5 @@ git fetch --all
 git pull origin
 git add $fignewname
 git commit -a -m "Update ICL result from SC in $(date +%Y-%m-%d-%T)"
-git push 
+git push >> $LOGFILE 2>&1
+echo "$(date +%Y-%m-$d-%T) : Finished" >> $LOGFILE 2>&1
